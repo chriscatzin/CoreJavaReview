abd900d7fb9a2ecefd7e39d57c8cbdc1e9fddecc
ckc196@humboldt.edu <ckc196@humboldt.edu>
2020-02-11 16:14:44 -0800

    Core Java

---
+++ src/com/collabera/ElementSkateboards.java
@@ -0,0 +1,9 @@
+package com.collabera;
+
+public class ElementSkateboards extends skateboard{
+
+    public ElementSkateboards(String name, int brain, int body, int size, int weight){
+        super(name, brain, body, size, weight);
+    }
+
+}
---
+++ src/com/collabera/skateboard.java
@@ -0,0 +1,38 @@
+package com.collabera;
+
+public class skateboard {
+
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public skateboard(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
