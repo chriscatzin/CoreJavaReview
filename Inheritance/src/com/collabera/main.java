afe03fd39eac0aee3344865e830814b8cd4bcde1
ckc196@humboldt.edu <ckc196@humboldt.edu>
2020-02-11 16:27:19 -0800

    Core Java

--- Inheritance/src/com/collabera/main.java
+++ Inheritance/src/com/collabera/main.java
@@ -3,6 +3,6 @@ package com.collabera;
 public class main {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        skateboard Skateboards = new skateboard();
     }
 }
