f2d5dc75326c0ab3e31423ef16c972e302fb1c92
ckc196@humboldt.edu <ckc196@humboldt.edu>
2020-02-19 13:47:26 -0800

    Core Java

--- EcapsulationReview/src/com/collabera/Main.java
+++ EcapsulationReview/src/com/collabera/Main.java
@@ -4,14 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player = new Player();
-        player.fullname = "Chris";
-        player.health = 20;
-        player.weapon = "Sword";
+        //Player player = new Player();
+        //player.fullname = "Chris";
+        //player.health = 20;
+        //player.weapon = "Sword";
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " +
-                player.healthRemaining());
+        //int damage = 10;
+        //player.loseHealth(damage);
+        //System.out.println("Remaining health = " +
+        //        player.healthRemaining());
+
+
+        EnchancedPlayer enchancedPlayer = new EnchancedPlayer("Chris", 200, "Sword");
+        System.out.println("Initial health is " + enchancedPlayer.getHealth());
+
     }
 }
